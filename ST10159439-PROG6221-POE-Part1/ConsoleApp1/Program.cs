556421bb69762bc40f0f9b47d88403bcbdf07d32
keadub <103220966+keadub@users.noreply.github.com>
2023-04-25 20:23:43 +0200

    Add files via upload

---
+++ ST10159439-PROG6221-POE-Part1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using ConsoleApp1;
+using System;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //input data to gather information from the user
+        recipeList recipe = new recipeList();
+
+        while (true)
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Enter recipe details");
+            Console.WriteLine("2. Display recipe");
+            Console.WriteLine("3. Scale recipe");
+            Console.WriteLine("4. Reset recipe");
+            Console.WriteLine("5. Clear recipe data");
+            Console.WriteLine("6. Quit");
+            Console.WriteLine("---------------------------");
+
+            string action = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (action)
+            {
+                case "1":
+                    recipe.DetailedList();
+                    break;
+
+                case "2":
+                    recipe.Display();
+                    break;
+
+                case "3":
+                    recipe.ScaleRecipe();
+                    break;
+
+                case "4":
+                    recipe.ResetInformation();
+                    break;
+
+                case "5":
+                    recipe.ClearRecipe();
+                    break;
+
+                case "6":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
+    }
+}
---
+++ ST10159439-PROG6221-POE-Part1/ConsoleApp1/recipeList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    public class recipeList
+    {
+            private int AmtOfIngredients = 0;
+            private string[] nameOfIngredient;
+            private double[] QuantitiesOfIngredients;
+            private string[] UnitsOfIngredient;
+            private int numSteps = 0;
+            private string[] steps;
+
+
+            public void DetailedList()
+            {
+            // Prompt the user to enter the number of ingredients
+
+            Console.Write("Enter number of ingredients: ");
+                AmtOfIngredients = Convert.ToInt32(Console.ReadLine());
+
+            // Initialize the arrays with the correct size
+                nameOfIngredient = new string[AmtOfIngredients];
+                QuantitiesOfIngredients = new double[AmtOfIngredients];
+                UnitsOfIngredient = new string[AmtOfIngredients];
+
+                for (int a = 0; a < AmtOfIngredients; a++)
+                {
+            // Prompt the user to enter the details for each ingredient
+
+                    Console.Write($"Enter ingredient {a+1 } name: ");
+                    nameOfIngredient[a] = Console.ReadLine();
+
+                    Console.Write($"Enter ingredient {a+1} quantity: ");
+                    QuantitiesOfIngredients[a] = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write($"Enter ingredient {a+1} unit: ");
+                    UnitsOfIngredient[a] = Console.ReadLine();
+                }
+            // Prompt the user to enter the number of steps
+
+                Console.Write("Enter number of steps: ");
+                numSteps = Convert.ToInt32(Console.ReadLine());
+
+            // Initialize the steps array with the correct size
+                steps = new string[numSteps];
+
+            for (int a = 0; a < numSteps; a++)
+            {
+                // Prompt the user to enter the details for each step
+                Console.WriteLine($"Enter step {a + 1}");
+                
+                  steps[a] = Console.ReadLine();
+                }
+
+                Console.WriteLine("details stored successfully.");
+            }
+
+
+            public void Display()
+            {
+            // Display the ingredients and quantities
+
+                Console.WriteLine("Recipe:");
+
+                for (int b = 0; b < AmtOfIngredients; b++)
+                {
+                Console.WriteLine(QuantitiesOfIngredients[b] + "  ingredient/s");
+                Console.WriteLine(UnitsOfIngredient[b]+" unit/s");
+                Console.WriteLine(nameOfIngredient[b]);
+                }
+
+            // Display the steps
+                for (int b = 0; b < numSteps; b++)
+                {
+                    Console.WriteLine($"Steps {b + 1}: {steps[b]}");
+                }
+
+            }
+
+            public void ScaleRecipe()
+            {
+            // Multiply all the quantities by the scaling factor
+                Console.Write("Enter scaling factor (0.5, 2, or 3): ");
+                double scalingFactor = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < AmtOfIngredients; i++)
+                {
+                    QuantitiesOfIngredients[i] *= scalingFactor;
+                }
+
+                Console.WriteLine("Recipe scaled successfully.");
+
+            }
+
+            public void ResetInformation()
+            {
+            // Reset all the quantities to their original values
+                for (int c = 0; c < AmtOfIngredients; c++)
+                {
+                    QuantitiesOfIngredients[c] /= 2;
+
+                }
+                Console.WriteLine("Reset successfully.");
+            }
+
+            public void ClearRecipe()
+            {
+            // Reset all the arrays to empty
+            Array.Clear(steps, 0, steps.Length);
+            Array.Clear(UnitsOfIngredient, 0, UnitsOfIngredient.Length);
+            Array.Clear(nameOfIngredient, 0, nameOfIngredient.Length);
+            Array.Clear(QuantitiesOfIngredients,0, QuantitiesOfIngredients.Length);
+            Console.WriteLine("infomation cleared");
+            }
+        }
+    }
+
